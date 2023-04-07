6bb8369d3ec36fd09c42feb92e218966e1fc5895
Florence <florencenantabak@gmail.com>
2023-04-07 11:25:22 +0300

    changes

--- routes/foRoutes.js
+++ routes/foRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const connectEnsureLogin = require("connect-ensure-login")
+const User= require("../models/userModel")
 
 
+router.get("/fodash",connectEnsureLogin.ensureLoggedIn(), async (req,res)=>{
+    let farmerWard= req.user["ward"]
+    console.log("farmer ward is:", farmerWard)
+    if(req.user.role == "fo"){
+        try{
+            let activeFarmers= await User.countDocuments();
+            let newUrbanFarmers= await User.find({role: "uf", ward: farmerWard})
 
-router.get("/fodash",connectEnsureLogin.ensureLoggedIn(), (req,res)=>{
-    res.render("foDash")
+            res.render("foDash", {
+                urbanFarmers: newUrbanFarmers,
+                activeFarmers})
+        }catch(error){
+            res.status(400).send("could not find framers in the database")
+        }
+        
+}else{res.redirect("/login")}
 });
 
 
